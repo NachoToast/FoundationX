62b0f8dd76a9e703e4ac915197d254004fa3cbd2
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-10-12 20:12:38 +1300

    latest scp images

--- src/components/GameShowcase/SecretLab.tsx
+++ src/components/GameShowcase/SecretLab.tsx
@@ -3,7 +3,9 @@ import imageA from '../../assets/secretLab/20230925151458_1.jpg';
 import imageB from '../../assets/secretLab/20230925151509_1.jpg';
 import imageC from '../../assets/secretLab/20230925151620_1.jpg';
 import imageD from '../../assets/secretLab/20230925155035_1.jpg';
+import imageG from '../../assets/secretLab/jordy.png';
 import imageF from '../../assets/secretLab/jumpergum.png';
+import imageH from '../../assets/secretLab/jumpergum2.png';
 import secretLabLogo from '../../assets/secretLab/logo.png';
 import imageE from '../../assets/secretLab/nathan.png';
 import ImageSlideshow from '../ImageSlideshow';
@@ -23,7 +25,7 @@ const SecretLab = () => {
                 <GameSectionInfo>
                     <img src={secretLabLogo} width="128px" />
                 </GameSectionInfo>
-                <ImageSlideshow imagesA={[imageE, imageA, imageB]} imagesB={[imageF, imageC, imageD]} />
+                <ImageSlideshow imagesA={[imageG, imageE, imageA, imageB]} imagesB={[imageH, imageF, imageC, imageD]} />
             </GameSectionContent>
         </GameSection>
     );
